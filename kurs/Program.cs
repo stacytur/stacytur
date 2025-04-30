5dd92662b300d1b969382115c983f9013ac6c155
stacytur <turanova.nastya@gmail.com>
2025-04-30 18:00:52 +0300

    Update Program.cs

--- kurs/Program.cs
+++ kurs/Program.cs
@@ -14,7 +14,7 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        
         builder.Services.AddControllersWithViews();
 
         // Add DbContext
